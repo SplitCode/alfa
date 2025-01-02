f5bd8473bae9f5befd20ea1761896aad6afd826d
Natalia Bandurova <natacsp@mail.ru>
2025-01-03 02:02:06 +0300

    feat: update router path

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,25 +1,24 @@
-import { createBrowserRouter } from 'react-router-dom';
+import { createBrowserRouter, Navigate } from 'react-router-dom';
 import ProductsList from '../components/ProductsList';
 
 export const router = createBrowserRouter(
   [
     {
       path: '/',
-      children: [
-        {
-          path: 'products',
-          element: <ProductsList />,
-        },
-        // {
-        //   path: 'products/:id',
-        //   element: <ProductDetail />,
-        // },
-        // {
-        //   path: 'create-product',
-        //   element: <ProductForm />,
-        // },
-      ],
+      element: <Navigate to="/products" replace />,
     },
+    {
+      path: 'products',
+      element: <ProductsList />,
+    },
+    // {
+    //   path: 'products/:id',
+    //   element: <ProductDetail />,
+    // },
+    // {
+    //   path: 'create-product',
+    //   element: <ProductForm />,
+    // },
   ],
   { future: { v7_startTransition: true } }
 );
