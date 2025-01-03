3983f6413156320e803e05507e5e2e6bdd23ee45
Natalia Bandurova <natacsp@mail.ru>
2025-01-03 03:38:57 +0300

    feat: add product details

---
+++ src/components/ProductDetail.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { useParams, useNavigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { RootState } from '../store/store';
+
+const ProductDetail: React.FC = () => {
+  const { id } = useParams<{ id: string }>();
+  const navigate = useNavigate();
+  const product = useSelector((state: RootState) =>
+    state.products.products.find(
+      (product) => product.id === parseInt(id || '', 10)
+    )
+  );
+
+  if (!product) {
+    return <div>Product not found</div>;
+  }
+
+  return (
+    <div>
+      <button onClick={() => navigate('/products')}>⬅ Back to Products</button>
+      <h1>{product.title}</h1>
+      <img
+        src={product.image}
+        alt={product.title}
+        style={{ width: '300px', height: '300px' }}
+      />
+      <p>{product.description}</p>
+    </div>
+  );
+};
+
+export default ProductDetail;
--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,5 +1,6 @@
 import { createBrowserRouter } from 'react-router-dom';
 import ProductsList from '../components/ProductsList';
+import ProductDetail from '../components/ProductDetail';
 
 export const router = createBrowserRouter(
   [
@@ -11,10 +12,10 @@ export const router = createBrowserRouter(
       path: 'products',
       element: <ProductsList />,
     },
-    // {
-    //   path: 'products/:id',
-    //   element: <ProductDetail />,
-    // },
+    {
+      path: 'products/:id',
+      element: <ProductDetail />,
+    },
     // {
     //   path: 'create-product',
     //   element: <ProductForm />,
