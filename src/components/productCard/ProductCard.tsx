090aed60a6d2be831e8d4c82e4cb80abbe1af729
Natalia Bandurova <natacsp@mail.ru>
2025-01-04 03:24:15 +0300

    feat: update card styles

--- src/components/productCard/ProductCard.tsx
+++ src/components/productCard/ProductCard.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
-import { toggleLike, removeProduct } from '../../store/productsSlice';
 import { useNavigate } from 'react-router-dom';
+import { toggleLike, removeProduct } from '../../store/productsSlice';
 import { Product } from '../../store/productsSlice';
-
+import './ProductCard.css';
 interface ProductCardProps {
   product: Product;
 }
@@ -12,32 +12,42 @@ const ProductCard: React.FC<ProductCardProps> = ({ product }) => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
-  const handleLike = () => dispatch(toggleLike(product.id));
-  const handleRemove = () => dispatch(removeProduct(product.id));
+  const handleLike = (e: React.MouseEvent) => {
+    e.stopPropagation();
+    dispatch(toggleLike(product.id));
+  };
+
+  const handleRemove = (e: React.MouseEvent) => {
+    e.stopPropagation();
+    dispatch(removeProduct(product.id));
+  };
+
   const handleCardClick = () => navigate(`/products/${product.id}`);
 
   return (
-    <div
-      style={{ border: '1px solid #ccc', padding: '10px', borderRadius: '8px' }}
-    >
-      <div onClick={handleCardClick} style={{ cursor: 'pointer' }}>
-        <h3>{product.title}</h3>
+    <div className="product-card" onClick={handleCardClick}>
+      <div className="product-card-body">
+        <h3>{product.title.slice(0, 20)}...</h3>
         <img
           src={product.image}
           alt={product.title}
-          style={{ width: '150px', height: '150px' }}
+          className="product-card-image"
         />
-        <p>Category: {product.category}</p>
-        <p>{product.description.slice(0, 35)}...</p>
+        <p className="product-card-category">Category: {product.category}</p>
+        <p className="product-card-description">
+          {product.description.slice(0, 50)}...
+        </p>
       </div>
-      <div>
+      <div className="product-card-actions">
         <button
           onClick={handleLike}
-          style={{ color: product.liked ? 'red' : 'black' }}
+          className={`product-card-like ${product.liked ? 'liked' : ''}`}
         >
           ❤️
         </button>
-        <button onClick={handleRemove}>❌</button>
+        <button onClick={handleRemove} className="product-card-remove">
+          ❌
+        </button>
       </div>
     </div>
   );
